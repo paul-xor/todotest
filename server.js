cdab5aa097acd3bdd39dd843091fbc601a2babf6
Pasha <palexsmol@gmail.com>
2020-05-08 16:30:24 -0700

    add env variables

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 
-const port = 7777;
+// console.log(process.env);
+if (process.env.NODE_ENV === 'development') {
+    console.log('NODE_ENV is on development mode');
+}
+
+const port = process.env.PORT || 7777;
 app.listen(port, () => {
     console.log(`Server running on PORT:${port} ...`);
 });
