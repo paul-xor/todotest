bd9662ebb7e8544121ef82f0022bc3eabb7b38c7
Pasha <palexsmol@gmail.com>
2020-05-10 16:52:06 -0700

    handling duplicate db fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,18 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -27,7 +42,6 @@ const sendErrorProd = (err, res) => {
         });
     }
 };
-
 module.exports = (err, req, res, next) => {
     // console.log(err.stack);
 
@@ -37,6 +51,13 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = { ...err };
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        // if (error.name === 'ValidationError')
+        //   error = handleValidationErrorDB(error);
+
+        sendErrorProd(error, res);
     }
 };
