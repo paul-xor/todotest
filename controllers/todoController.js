e512038bef1fd6dc2982323a7c57af256939a011
Pasha <palexsmol@gmail.com>
2020-05-10 16:38:51 -0700

    add 404 Not Found Errors

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const Todo = require('./../models/todoModel');
 const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTodo = (req, res, next) => {
   req.query.limit = '5';
@@ -9,145 +11,116 @@ exports.aliasTopTodo = (req, res, next) => {
   next();
 };
 
-exports.getAllTodos = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Todo.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const todos = await features.query;
+exports.getAllTodos = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Todo.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const todos = await features.query;
 
-    // SEND RESPONCE
-    res.status(200).json({
-      status: 'success',
-      requestedAt: req.requestTime,
-      results: todos.length,
-      data: {
-        todos
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
+  // SEND RESPONCE
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: todos.length,
+    data: {
+      todos
+    }
+  });
+});
+
+exports.getTodo = catchAsync(async (req, res, next) => {
+  const todo = await Todo.findById(req.params.id);
+  // Todo.findOne({ _id: req.params.id })
+
+  if (!todo) {
+    return next(new AppError('No todo found with that ID', 404));
   }
-};
 
-exports.getTodo = async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      todo
+    }
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        todo
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
+exports.createTodo = catchAsync(async (req, res, next) => {
+  const newTodo = await Todo.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      todo: newTodo
+    }
+  });
+});
+
+exports.updateTodo = catchAsync(async (req, res, next) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!todo) {
+    return next(new AppError('No todo found with that ID', 404));
   }
-};
 
-exports.createTodo = async (req, res) => {
-  try {
-    const newTodo = await Todo.create(req.body);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      todo: todo
+    }
+  });
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: {
-        todo: newTodo
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err
-    });
+exports.deleteTodo = catchAsync(async (req, res, next) => {
+  const todo = await Todo.findByIdAndDelete(req.params.id);
+
+  if (!todo) {
+    return next(new AppError('No todo found with that ID', 404));
   }
-};
 
-exports.updateTodo = async (req, res) => {
-  try {
-    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        todo: todo
+exports.getTodoStats = catchAsync(async (req, res, next) => {
+  const stats = await Todo.aggregate([
+    {
+      $match: { ratingAverage: { $gte: 4.5 } }
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' },
+        numTodos: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
       }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data send!'
-    });
-  }
-};
+    },
+    {
+      $sort: { avgPrice: 1 }
+    }
+    // {
+    //   $match: { _id: { $ne: 'EASY' } }
+    // }
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats
+    }
+  });
+});
 
-exports.deleteTodo = async (req, res) => {
-  try {
-    await Todo.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data send!'
-    });
-  }
-};
-
-
-exports.getTodoStats = async (req, res) => {
-  try {
-    const stats = await Todo.aggregate([
-      {
-        $match: { ratingAverage: { $gte: 4.5 } }
-      },
-      {
-        // $group: {
-        //   _id: { $toUpper: '$difficulty' },
-        //   numTodos: { $sum: 1 },
-        //   numRatings: { $sum: '$ratingsQuantity' },
-        //   avgRating: { $avg: '$ratingsAverage' },
-        //   avgPrice: { $avg: '$price' },
-        //   minPrice: { $min: '$price' },
-        //   maxPrice: { $max: '$price' }
-        // }
-      },
-      {
-        $sort: { avgPrice: 1 }
-      }
-      // {
-      //   $match: { _id: { $ne: 'EASY' } }
-      // }
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
-
-exports.getMonthlyPlan = async (req, res, next) => {
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   const year = req.params.year * 1; // 2021
 
   const plan = await Todo.aggregate([
@@ -191,4 +164,4 @@ exports.getMonthlyPlan = async (req, res, next) => {
       plan
     }
   });
-};
+});
